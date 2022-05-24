14940398962e2adcf8d8c1b91b2b4df3db2e3102
grantkee <grant@kee.ai>
2022-05-24 09:27:45 -0500

    scaffold kitty struct - enum for Gender

--- kitty-node/pallets/kitties/src/lib.rs
+++ kitty-node/pallets/kitties/src/lib.rs
@@ -4,18 +4,42 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-    use frame_support::{
-        sp_runtime::traits::{Hash, Zero},
+	use frame_support::{
+		sp_runtime::traits::{Hash, Zero},
         dispatch::{DispatchResultWithPostInfo, DispatchResult},
         traits::{Currency, ExistenceRequirement, Randomness},
         pallet_prelude::*
     };
     use frame_system::pallet_prelude::*;
     use sp_io::hashing::blake2_128;
+	use scale_info::TypeInfo;
 
-    // TODO Part II: Struct for holding Kitty information.
 
-    // TODO Part II: Enum and implementation to handle Gender type in Kitty struct.
+    #[cfg(feature = "std")]
+    use frame_support::serde::{Deserialize, Serialize};
+
+	type AccountOf<T> = <T as frame_system::Config>::AccountId;
+	type BalanceOf<T> = <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+	// Struct for individual Kitty info
+	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	#[scale_info(skip_type_params(T))]
+	#[codec(mel_bound())]
+	pub struct Kitty<T: Config> {
+		pub dna: [u8; 16],
+		pub price: Option<BalanceOf<T>>,
+		pub gender: Gender,
+		pub owner: AccountOf<T>,
+	}
+
+	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+	pub enum Gender {
+		Male,
+		Female,
+	}
+
+    // ACTION #3: Implementation to handle Gender type in Kitty struct.
 
     #[pallet::pallet]
     #[pallet::generate_store(pub(super) trait Store)]
@@ -31,6 +55,11 @@ pub mod pallet {
         type Currency: Currency<Self::AccountId>;
 
         // TODO Part II: Specify the custom types for our runtime.
+
+
+        // ACTION #5: Specify the type for Randomness we want to specify for runtime.
+
+        // ACTION #9: Add MaxKittyOwned constant
 
     }
 
@@ -52,30 +81,36 @@ pub mod pallet {
 	/// Keeps track of the total number of Kitties in existence.
 	pub(super) type CountForKitties<T: Config> = StorageValue<_, u64, ValueQuery>;
 
-    // TODO Part II: Remaining storage items.
+    // ACTION #7: Remaining storage items.
 
-    // TODO Part III: Our pallet's genesis configuration.
+    // TODO Part IV: Our pallet's genesis configuration.
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
 
         // TODO Part III: create_kitty
 
-        // TODO Part III: set_price
+        // TODO Part IV: set_price
 
-        // TODO Part III: transfer
+        // TODO Part IV: transfer
 
-        // TODO Part III: buy_kitty
+        // TODO Part IV: buy_kitty
 
-        // TODO Part III: breed_kitty
+        // TODO Part IV: breed_kitty
     }
 
-    // TODO Part II: helper function for Kitty struct
+    //** Our helper functions.**//
 
     impl<T: Config> Pallet<T> {
+
+        // ACTION #4: helper function for Kitty struct
+
         // TODO Part III: helper functions for dispatchable functions
 
-        // TODO: increment_nonce, random_hash, mint, transfer_from
+        // ACTION #6: function to randomly generate DNA
 
+        // TODO Part III: mint
+
+        // TODO Part IV: transfer_kitty_to
     }
 }
